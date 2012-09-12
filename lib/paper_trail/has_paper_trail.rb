59b5767b601e48d2089585d4811a8059c10af61f
Jens Norrgrann <jens.norrgrann@bukowskis.com>
2012-09-12 16:13:14 +0200

    Serialize to json instead of yaml
    Had weird errors with yaml...

--- lib/paper_trail/has_paper_trail.rb
+++ lib/paper_trail/has_paper_trail.rb
@@ -164,7 +164,7 @@ module PaperTrail
             # The double negative (reject, !include?) preserves the hash structure of self.changes.
             data[:object_changes] = self.changes.reject do |key, value|
               !notably_changed.include?(key)
-            end.to_yaml
+            end.to_json
           end
           send(self.class.versions_association_name).build merge_metadata(data)
         end
@@ -205,7 +205,7 @@ module PaperTrail
       end
 
       def object_to_string(object)
-        object.attributes.except(*self.class.skip).to_yaml
+        object.attributes.except(*self.class.skip).to_json
       end
 
       def changed_notably?
